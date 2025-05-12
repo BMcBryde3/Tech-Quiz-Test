3f9b3570ecddf13dd7459c95b6b986eed3c4953b
Bread <brett.mcbryde@yahoo.com>
2025-05-12 14:23:37 -0500

    Code.

---
+++ cypress.config.ts
@@ -0,0 +1,29 @@
+
+import { defineConfig } from 'cypress';
+import customViteConfig from './vite.config';
+
+export default defineConfig({
+  component: {
+
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+      viteConfig: {
+        ...customViteConfig,
+        server: {
+          ...(customViteConfig.server || {}),
+          port: 3002,
+          host: 'localhost',
+        },
+      }
+    },
+    specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
